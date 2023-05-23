d07a52b51fdc37ac2fb16c4f93e12a061295d6d4
DregerTi <72619834+DregerTi@users.noreply.github.com>
2023-05-23 18:22:51 +0200

    create entity toDoList

---
+++ entities/ToDoList.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Entities;
+use http\Exception\RuntimeException;
+
+class ToDoList
+{
+    private int $idUser;
+    public int $id;
+    function __construct(int $idUser, int $id)
+    {
+        $this->idUser = $idUser;
+        $this->id = $id;
+    }
+
+    public function getIdUser(): int
+    {
+        return $this->idUser;
+    }
+
+    public function setIdUser(int $idUser): void
+    {
+        $this->idUser = $idUser;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function addItem($item): void
+    {
+        $totalItems = $this->totalItems();
+
+        if ($totalItems === 7){
+            //send email
+        }
+        if ($totalItems === 10){
+            throw new RuntimeException('To many items in this toDoList')
+        }
+        $this->items[] = $item;
+    }
+
+    public function allItems(): array
+    {
+        return $this->items;
+    }
+
+    public function totalItems(): int
+    {
+        return sizeof($this->items);
+    }
+    
+}
